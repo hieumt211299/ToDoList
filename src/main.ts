fc2968e23d9527e875db9ca5b084f18950edef43
Mai Trung Hieu <hieumt211299@gmail.com>
2023-06-20 09:28:06 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
+import { vue } from '@vitejs/plugin-vue';
 import { createApp } from 'vue'
-import 'style.css'
-import App from 'App.vue'
+import './style.css'
+import App from './App.vue'
 
 createApp(App).mount('#app')
